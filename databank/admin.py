763c0877217477915483afb3a9270a9d12f18e73
Your Name <you@example.com>
2018-10-24 09:52:10 +0100

    import export

--- databank/admin.py
+++ databank/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Study
-from .models import Participant
-from .models import Result
+from .models import *
+from import_export.admin import ImportExportModelAdmin
+
 # Register your models here.
+
+@admin.register(Result)
+class ResultsAdmin(ImportExportModelAdmin):
+    pass
 
 admin.site.register(Study)
 admin.site.register(Participant)
-admin.site.register(Result)
